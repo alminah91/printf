a55ddca274496869d8e63e51d0aa8c125c8140fb
alminah91 <motlhabanialminah@gmail.com>
2023-09-18 16:17:03 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,31 +1,36 @@
 #include"main.h"
 #include <stdarg.h>
 #include<string.h>
-
+/**
+ * _printf - print formated text
+ * @format: format specifier
+ * Return: number of bytes printed
+ */
 int _printf(const char *format, ...)
 {
-        int i, j = 0;
-        va_list args;
-        va_start(args, format);
+	int i, j = 0;
 
-        for(i = 0;format[i] != '\0'; i++)
-        {
-                if (format[i] != '%')
-                {
-                        _putchar(format[i]);
-                }
-                else if (format[i + 1] == 'c')
-                {
-                        _putchar(va_arg(args, int));
-                        i++;
-                }
-                else if (format[i + 1] == '%')
-                {
-                        _putchar('%');
-                }
-                j += 1;
+	va_list args;
 
-        }
-        va_end(args);
-        return (j);
+	va_start(args, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	i{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			_putchar(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == '%')
+		{
+			_putchar('%');
+		}
+		j += 1;
+	}
+	va_end(args);
+	return (j);
 }
